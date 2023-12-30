5241ce4e23492c579ba3bda353b1a61bf2456ef9
李想 <2021010763@secoder.net>
2023-12-30 11:55:18 +0800

    feat: our model

---
+++ projects/mmdet3d_plugin/voxformer/dense_heads/__init__.py
@@ -0,0 +1,2 @@
+from .voxformer_head import VoxFormerHead
+from .voxformer_head_ms import VoxFormerHead_ms
---
+++ projects/mmdet3d_plugin/voxformer/modules/__init__.py
@@ -0,0 +1,9 @@
+from .transformer import PerceptionTransformer
+from .encoder import VoxFormerEncoder, VoxFormerLayer
+from .deformable_cross_attention import DeformCrossAttention, MSDeformableAttention3D
+from .deformable_self_attention import DeformSelfAttention
+from .cvt import CVT
+from .tvt import TVT
+from .sem import Sem_Decoder
+from .depth import Depth_Decoder
+from .large_model import Large_Decoder
